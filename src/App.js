c5560205210bf2e90c4f71203890756194896d7d
Mariam <Mariamhh98@hotmail.com>
2019-09-23 11:43:32 +0300

    Mariam and Samira

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from "react";
 
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    authors: [],
+    filteredAuthors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  async componentDidMount() {
+    try {
+      let response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      let authors = response.data;
+      this.setState({
+        loading: false,
+        authors: authors,
+        filteredAuthors: authors
+      });
+      console.log(authors);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  checkLoading = () => {
+    if (this.state.loading) {
+      return <Loading />;
+    } else {
+      return <div className="content col-10">{this.getContentView()}</div>;
+    }
+  };
+
+  selectAuthor = async author => {
+    const authorID = author.id;
+    console.log(authorID);
+    this.setState({ loading: true });
+    try {
+      let response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${authorID}`
+      );
+      let author = response.data;
+      this.setState({
+        currentAuthor: author,
+        loading: false
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -48,7 +92,7 @@ class App extends Component {
           <div className="col-2">
             <Sidebar unselectAuthor={this.unselectAuthor} />
           </div>
-          <div className="content col-10">{this.getContentView()}</div>
+          <div className="content col-10">{this.checkLoading()}</div>;
         </div>
       </div>
     );
--- src/BookRow.js
+++ src/BookRow.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function BookRow(props) {
   const book = props.book;
-  const author = props.author;
+  console.log("book: " + book);
+  const authors = book.authors;
+  console.log("autors: " + authors);
+
+  const auth = authors.map(author => <> {author.name} </>);
   return (
     <tr>
       <td>{book.title}</td>
-      <td>
-        {author.first_name} {author.last_name}
-      </td>
+      <td> {auth} </td>
       <td>
         <button className="btn" style={{ backgroundColor: book.color }} />
       </td>
